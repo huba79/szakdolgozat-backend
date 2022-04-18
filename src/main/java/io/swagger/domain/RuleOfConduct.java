2af22697e1326c0028ba13e0f768299a301694ce
huba <tanczos.huba@gmail.com>
2022-04-18 14:52:06 +0300

    Rules entity done

---
+++ src/main/java/io/swagger/domain/RuleOfConduct.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package io.swagger.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author huba
+ */
+@Entity
+@Table(name="rules_of_conduct")
+public class RuleOfConduct implements Serializable {
+
+    private @Id @GeneratedValue(strategy=GenerationType.AUTO)
+    Long id;
+    
+    @Column(name="COMPANY_ID", columnDefinition= "BIGINT NOT NULL",length=10)
+    Long companyId;
+   
+    @Column(name="lake_id",columnDefinition="BIGINT NOT NULL")
+    Long lakeId ;  
+    
+    @Column(name="RULE_TEXT",columnDefinition="VARCHAR(3072) NOT NULL")
+    String ruleText;  
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Long companyId) {
+        this.companyId = companyId;
+    }
+
+    public Long getLakeId() {
+        return lakeId;
+    }
+
+    public void setLakeId(Long lakeId) {
+        this.lakeId = lakeId;
+    }
+
+    public String getRuleText() {
+        return ruleText;
+    }
+
+    public void setRuleText(String ruleText) {
+        this.ruleText = ruleText;
+    }       
+    
+}
