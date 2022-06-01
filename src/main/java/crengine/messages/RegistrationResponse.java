2ca97e77ebcb8578f1bccdf5250cbd7ef1fdf43e
huba <tanczos.huba@gmail.com>
2022-06-01 13:43:33 +0300

    refactoring base packages

---
+++ src/main/java/crengine/messages/RegistrationResponse.java
@@ -0,0 +1,92 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package crengine.messages;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.io.Serializable;
+
+/**
+ *
+ * @author huba
+ */
+public class RegistrationResponse implements Serializable   {
+    @JsonProperty
+    private Long id;
+    
+    @JsonProperty("displayName")
+    String displayName;
+
+    @JsonProperty("nickName")
+    String nickName;
+
+    @JsonProperty("emailAddress")
+    String emailAddress;
+
+    @JsonProperty("groupId")
+    String groupId;
+
+    @JsonProperty("companyId")
+    Long companyId;
+
+    @JsonProperty("sessionID")
+    String sessionID;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+
+    public Long getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Long companyId) {
+        this.companyId = companyId;
+    }
+
+    public String getSessionID() {
+        return sessionID;
+    }
+
+    public void setSessionID(String sessionID) {
+        this.sessionID = sessionID;
+    }
+    
+}
